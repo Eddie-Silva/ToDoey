11eb3d12c7bcf0fc5f5a750f6478a5c147df2c11
Taylor Batch <tbatch27@taylors-macbook-pro.local>
2018-09-11 10:13:43 -0500

    Convert to MVC patern

---
+++ ToDoey/DataModel/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Taylor Batch on 9/11/18.
+//  Copyright © 2018 burgeoning. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+   var title: String = ""
+   var done: Bool = false
+   
+//   init(item: String, checked: Bool) {
+//      self.title = item
+//      self.done = checked
+//   }
+}
