4b48406bf69bc7d7b4dd8bf1d14ebeda6e419828
Taylor Batch <tbatch27@taylors-macbook-pro.local>
2018-09-14 15:45:08 -0500

    swipe delete to items VC

--- ToDoey/Controller/TodoListViewController.swift
+++ ToDoey/Controller/TodoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class TodoListViewController: UITableViewController {
+class TodoListViewController: SwipeTableViewController {
 
    
    var todoItems: Results<Item>?
@@ -33,7 +33,8 @@ class TodoListViewController: UITableViewController {
    }
    
    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+      
+     let cell = super.tableView(tableView, cellForRowAt: indexPath)
       
       if let item = todoItems?[indexPath.row] {
          cell.textLabel?.text = item.title
@@ -43,8 +44,6 @@ class TodoListViewController: UITableViewController {
          cell.textLabel?.text = "No Items"
       
       }
-      
-      
       
       return cell
    }
@@ -115,6 +114,24 @@ class TodoListViewController: UITableViewController {
       
       tableView.reloadData()
    }
+   
+   override func updateModel(at indexPath: IndexPath) {
+      
+      //calls from superclass
+      //super.updateModel(at: indexPath)
+      
+      if let itemForDeletion = todoItems?[indexPath.row] {
+         do {
+            try self.realm.write {
+               self.realm.delete(itemForDeletion)
+            }
+         } catch {
+            print("ERROR \(error)")
+         }
+      }
+   }
+   
+   
    
 
 }
